6979211fd191ac931cd6e32edaf8ac8804e2b1da
ira089 <gvir2015@gmail.com>
2023-11-19 20:08:40 +0200

    js10_3

--- src/index.js
+++ src/index.js
@@ -15,10 +15,15 @@ console.dir(refs.selectCheck);
 refs.selectCheck.addEventListener('change', onChange);
 
 function onChange(evt) {
-  console.log(evt);
+  // console.log({ evt });
 
-  const changeEvt = evt.target.textContent;
-  console.log(changeEvt);
+  const selectedId = evt.currentTarget.value;
+  console.log(selectedId);
+
+  funFetchCatByBreed(selectedId).then(data => {
+    console.log(data);
+    refs.container.innerHTML = createMarkup(data);
+  });
 }
 
 console.log(funFetchBreeds());
@@ -26,23 +31,21 @@ console.log(funFetchBreeds());
 funFetchBreeds()
   .then(data => {
     console.dir(...data);
-    console.dir({ ...data });
-    // console.log({ ...data.name });
-
+    console.dir({ data });
     dataImg = data.filter(img => img.image?.url != null);
-
-    // console.log(dataImg);
-
     for (let i = 0; i < dataImg.length; i++) {
       const breed = dataImg[i];
       let option = document.createElement('option');
       if (!breed.image) {
         continue;
       }
-      option.value = i;
-      option.innerHTML = `${breed.name}`;
+      option.value = breed.id;
+      option.innerHTML = breed.name;
       refs.selectCheck.append(option);
     }
+
+    // console.log(dataImg);
+
     showFirstBreed(0);
     //написать функцию - показывать первую породу по умолчанию
   })
@@ -62,23 +65,17 @@ function showFirstBreed() {}
 // let imgCat = dataImg[index].image.url;
 // console.log(imgCat);
 
-// function createMarkup(arr) {
-//   return arr
-//     .map(      ( poster_path, release_date, original_title, vote_average )  => `
-//              <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}" />
-//                <div class="cat-info-text">
-//                   <h2>${original_title}</h2>
-//                  <p> ${release_date}</p>
-//                   <h3> ${vote_average}</h3>
-//                   <p> ${release_date}</p>
-//                </div>
-
-//     )    .join('');
-//     }
 function createMarkup(arr) {
-  return arr
+  const breeds = arr[0].breeds;
+  const imageUrl = arr[0].url;
+  return breeds
     .map(
-      () => `<img src="https://api.thecatapi.com/v1/images/search?breed_ids=${catId}" alt="${name}" />
+      ({
+        name,
+        temperament,
+        description,
+        id,
+      }) => `<img src="${imageUrl}" alt="${name}" />
                   <div class="cat-info-text">
                    <h2>${name}</h2>
                   <p> ${description}</p>
